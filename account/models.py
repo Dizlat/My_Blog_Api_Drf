6a3f98234e9070d72bac2d3cbd5b7ff5cc4cb03f
Dizlat <dizlat.ur@gmail.com>
2021-06-21 11:12:55 +0600

    start and myaccount

---
+++ account/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class MyUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=False)
+    activation_code = models.CharField(max_length=50, blank=True)
+
+#TODO: create activation code
+
