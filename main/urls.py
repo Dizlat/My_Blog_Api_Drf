d677275eaf29f4a61b7ce1592d3a28b1c3e93945
Dizlat <dizlat.ur@gmail.com>
2021-06-22 11:12:44 +0600

    genericApi

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from main.views import *
 
 urlpatterns = [
-    path('categories/', categories, name='categories-list'),
+    path('categories/', CategoryListView.as_view(), name='categories-list'),
     path('posts/', PostListView.as_view(), name='posts-list'),
+    path('post-create/', PostCreateView.as_view(), name='post-create'),
 
 ]
--- main/views.py
+++ main/views.py
@@ -2,28 +2,44 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import generics
 
 from main.models import *
 from main.serializers import *
 
 
-@api_view(['GET'])
-def categories(request):
-    if request.method == 'GET':
-        categories = Category.objects.all()
-        serializer = CategorySerializer(categories, many=True)
-        return Response(serializer.data)
+# @api_view(['GET'])
+# def categories(request):
+#     if request.method == 'GET':
+#         categories = Category.objects.all()
+#         serializer = CategorySerializer(categories, many=True)
+#         return Response(serializer.data)
+#
+#
+# class PostListView(APIView):
+#     def get(self, request):
+#         posts = Post.objects.all()
+#         serializer = PostSerializer(posts, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request):
+#         post = request.data
+#         serializer = PostSerializer(data=post)
+#         if serializer.is_valid(raise_exception=True):
+#             post_saved = serializer.save()
+#         return Response(serializer.data)
 
 
-class PostListView(APIView):
-    def get(self, request):
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
+class CategoryListView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
 
-    def post(self, request):
-        post = request.data
-        serializer = PostSerializer(data=post)
-        if serializer.is_valid(raise_exception=True):
-            post_saved = serializer.save()
-        return Response(serializer.data)
+
+class PostListView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostCreateView(generics.CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
