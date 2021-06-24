b422ab461de16fcdea359d06285483541539dab4
Dizlat <dizlat.ur@gmail.com>
2021-06-24 14:22:29 +0600

    activate url

--- account/urls.py
+++ account/urls.py
@@ -5,5 +5,5 @@ from .views import *
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
-    path('activate/str:activation_code>/', ActivateView.as_view())
+    path('activate/<str:activation_code>/', ActivateView.as_view())
 ]
